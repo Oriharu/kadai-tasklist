9192b73e7625447935998105cd75edfd6c4463d7
HARU <hahntb0501@gmail.com>
2017-11-11 10:35:11 +0000

    kadai-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,34 +1,37 @@
 class TasksController < ApplicationController
-before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:new, :create]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @task = Task.order(created_at: :desc).page(params[:page]).per(2)
+    if logged_in?
+      @tasks = current_user.tasks.all.page(params[:page]).per(10)
+    end
   end
-  
+
   def show
-    
   end
-  
+
   def new
     @task = Task.new
   end
-  
+
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に作成されました'
+      flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が作成されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
+
   def edit
-    
   end
-  
+
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -36,21 +39,26 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
-  end  
+  end
+
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-private
+  private
+
   def set_task
     @task = Task.find(params[:id])
   end
-  # Strong Parameter
+
+  def correct_user
+    redirect_to root_url if @task.user != current_user
+  end
+
   def task_params
     params.require(:task).permit(:content, :status)
   end
+end
